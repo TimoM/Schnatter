1a12df2181a0491d4209b8befd61551c8b758419
Timo Montalto <hallo@timomontalto.de>
2012-03-17 15:58:43 +0100

    Revert "Version 0.1"
    This reverts commit a4181fb14f0939f012af934a9bb31126fb54a735.

--- template/files/js/scripts.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-  hljs.tabReplace = '    ';
-  hljs.initHighlightingOnLoad();
-});
--- template/pieces/footer.php
+++
@@ -1,8 +0,0 @@
-  <footer>
-  	<?php echo stat::nav(); ?>
-  </footer>
-<div class="bar"></div>
-
-</body>
-
-</html>
